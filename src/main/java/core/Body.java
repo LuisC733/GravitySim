7d16525171e2130cb0c8c436c5e3bb39b2f9b755
Luis <lahrluis@icloud.com>
2026-03-06 15:17:10 +0100

    feat: vector and body class

--- src/main/java/core/Body.java
+++ src/main/java/core/Body.java
@@ -1,5 +1,20 @@
 package core;
 
 class Body{
-    //
+    public Vector pos;
+    public Vector velo;
+    public Vector accl;
+    public final double mass;
+    public double radius;
+
+    public Body(Vector pos, Vector velo, double mass, double radius){
+        this.pos = pos;
+        this.velo = velo;
+        this.mass = mass;
+        this.radius = radius;
+        this.accl = new Vector(0,0);
+    }
+    void applyForce(Vector f){
+        this.accl = f.scale(1.0/mass);
+    }
 }
--- src/main/java/core/Vector.java
+++ src/main/java/core/Vector.java
@@ -1,5 +1,28 @@
 package core;
 
-class Vector{
-    //
+public class Vector{
+
+    public final double x;
+    public final double y;
+
+    Vector(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public Vector add(Vector other){
+        return new Vector(this.x + other.x, this.y + other.y);
+    }
+    public Vector scale(double factor){
+        return new Vector(this.x * factor, this.y * factor);
+    }
+    public double magnitude(){
+        return Math.sqrt(this.x * this.x + this.y * this.y);
+    }
+    public Vector normalize(){
+        return new Vector(this.x / magnitude(), this.y / magnitude());
+    }
+    public Vector sub(Vector other){
+        return new Vector(this.x - other.x, this.y - other.y);
+    }
 }
