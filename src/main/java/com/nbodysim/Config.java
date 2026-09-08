0812e217d1f8f531e4e7aff49aafe76f44231063
Luis <lahrluis@icloud.com>
2026-09-08 17:10:54 +0200

    perf: raise default theta to 1.0

--- src/main/java/com/nbodysim/Config.java
+++ src/main/java/com/nbodysim/Config.java
@@ -6,6 +6,6 @@ public class Config {
 
     public static final double SIM_DT = 3600;
     public static double SCALE = 1e10;
-    public static final double THETA = 0.5;
+    public static final double THETA = 1.0;
     public static final double TOLERANCE = 1e-12;
 }
